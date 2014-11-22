1bc4c21c9b47b7feceda7e46b721c9e8dd4b2806
anotherbugmaster <anotherbugmaster@gmail.com>
2014-11-22 16:49:47 +0500

    Some things are working

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using fractals;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            ClientSize = new Size(500, 500);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            var polynom = new Polynom(2, new Complex(-0.1, 0.1));
+            foreach (var complex in FractalGen.getFractalComplexes(polynom))
+            {
+                var point = GetScreenCoords(complex, 2);
+                var brush = new SolidBrush(Color.Black);
+                graphics.FillEllipse(brush, point.X, point.Y, 3, 3);
+            }
+        }
+
+        private Point GetScreenCoords(Complex complex, double border)
+        {
+            var x = (complex.Real + border) / (2 * border) * ClientSize.Width;
+            var y = (complex.Imaginary + border) / (2 * border) * ClientSize.Height;
+            return new Point((int)x, (int)y);
+        }
+    }
+}
---
+++ fractals/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace fractals
+{
+    public class Polynom
+    {
+        private List<Double> coefficients;
+        private Complex freeCoefficient;
+
+        public static void Main(string[] args)
+        {
+            
+        }
+        public Polynom(List<Double> coefficients, Complex freeCoef)
+        {
+            this.coefficients = coefficients;
+            freeCoefficient = freeCoef;
+        }
+
+        public Polynom(int power, Complex freeCoef)
+        {
+            coefficients = Enumerable
+                .Range(0, power)
+                .Select(z => 0.0)
+                .ToList();
+            coefficients[power - 1] = 1;
+            freeCoefficient = freeCoef;
+        }
+
+        public Complex GetValue(Complex c)
+        {
+            var result = new Complex();
+            var power = c;
+            foreach (var coef in coefficients)
+            {
+                result += coef*power;
+                power *= c;
+            }
+            result += freeCoefficient;
+            return result;
+        }
+    }
+
+    public class FractalGen
+    {
+
+        private static bool isFractalPoint(Complex complex, Polynom function, double border)
+        {
+            var iterations = 50;
+            var z = function.GetValue(complex);
+            for (var i = 0; i < iterations; i++)
+            {
+                if (z.Magnitude >= border)
+                    return false;
+                z = function.GetValue(z);
+            }
+            return true;
+        }
+
+        public static IEnumerable<Complex> getFractalComplexes(Polynom function)
+        {
+            double border = 2;
+            double step = 0.01;
+            for (var x = -border; x <= border; x += step)
+                for (var y = -border; y <= border; y += step)
+                {
+                    var complex = new Complex(x, y);
+                    if (isFractalPoint(complex, function, border))
+                        yield return complex;
+                }
+        }
+    }
+
+
+}
